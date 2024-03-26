4f681b40b3a964a248c4920bd3177c0ea46030ed
mitrakov <mitrakov-artem@yandex.ru>
2024-03-26 22:37:21 +0300

    Add "New" vs. "Update" check

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:markdown_widget/widget/markdown.dart';
 import 'package:tommynotes/db.dart';
+import 'package:tommynotes/note.dart';
 import 'package:tommynotes/settings.dart';
 import 'package:tommynotes/trixcontainer.dart';
 
 const String lastOpenPathKey = "LAST_OPEN_PATH";
 
 void main() async {
+  // debugPaintSizeEnabled = true;
   WidgetsFlutterBinding.ensureInitialized(); // allow async code in main()
   await Settings.instance.init();
   final path = Settings.instance.settings.getString(lastOpenPathKey) ?? await getStartFile();
@@ -32,12 +34,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
+      title: "Tommy Notes",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: "Flutter Demo Home Page"),
+      home: const MyHomePage(title: "TommyNotes"),
     );
   }
 }
@@ -53,11 +56,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _ctrl = TextEditingController();
+  int _noteId = 0;
 
-  Future<Iterable<String>> getNotes() async {
+  Future<Iterable<Note>> getNotes() async {
     final db = Db.instance.database;
-    final dbResult = await db.rawQuery("SELECT data FROM note ORDER BY note_id;");
-    return dbResult.map((e) => e["data"].toString());
+    final dbResult = await db.rawQuery("SELECT note_id, data FROM note ORDER BY note_id;");
+    return dbResult.map((e) => Note(noteId: int.parse(e["note_id"].toString()), note: e["data"].toString()));
   }
 
   @override
@@ -66,6 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: IconButton(icon: const Icon(Icons.add_box_rounded), onPressed: () => setState(() {
+          _ctrl.text = "";
+          _noteId = 0;
+        })),
       ),
       body: Column(
         children: [
@@ -102,13 +110,21 @@ class _MyHomePageState extends State<MyHomePage> {
                       TrixContainer(
                         child: Row(children: [
                           OutlinedButton(onPressed: () {}, child: const Text("Tags here                                 ")),
-                          OutlinedButton(onPressed: () async {
-                            var data = _ctrl.text.trim();
+                          OutlinedButton(child: Text(_noteId == 0 ? "Add New" : "Save"), onPressed: () async {
+                            final data = _ctrl.text.trim();
                             if (data.isNotEmpty) {
-                              await Db.instance.database.rawInsert("INSERT INTO note (data) VALUES (?)", [data]); // returns primaryKey ID
-                              setState(() => _ctrl.text = "");
+                              if (_noteId == 0) { // INSERT
+                                final newId = await Db.instance.database.rawInsert("INSERT INTO note (data) VALUES (?);", [data]);
+                                setState(() {
+                                  _noteId = newId;
+                                });
+                              } else { // UPDATE
+                                await Db.instance.database.rawUpdate("UPDATE note SET data = ? WHERE note_id = ?;", [data, _noteId]);
+                                setState(() {}); // repaint
+                              }
+                              // TODO show OK dialog
                             }
-                          }, child: const Text("Save")),
+                          }),
                         ]),
                       ),
                     ],
@@ -124,9 +140,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     final futureResult = snapshot.data!;
-                    final children = futureResult.map((e) =>
+                    final children = futureResult.map((note) =>
                       TrixContainer(
-                        child: TextButton(onPressed: () => setState(() => _ctrl.text = e), child: Text(e.substring(0, min(e.length, 32))))
+                        child: TextButton(child: Text(note.note.substring(0, min(note.note.length, 32))), onPressed: () => setState(() {
+                          _ctrl.text = note.note;
+                          _noteId = note.noteId;
+                        }))
                       )).toList(); // TODO 32 is total char count which is wrong
                     return ListView(scrollDirection: Axis.horizontal, children: children); // TODO use ListView.builder
                   } else return const CircularProgressIndicator();
---
+++ lib/note.dart
@@ -0,0 +1,6 @@
+class Note {
+  final int noteId;
+  final String note;
+
+  Note({required this.noteId, required this.note});
+}
