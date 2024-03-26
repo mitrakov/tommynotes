9caddc8ca6a312d1164ad757c6da2d1f32ac11d7
mitrakov <mitrakov-artem@yandex.ru>
2024-03-27 02:28:37 +0300

    Add "Open DB" and "Create DB"

--- lib/db.dart
+++ lib/db.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: curly_braces_in_flow_control_structures
 import 'package:sqflite/sqflite.dart';
 
 class Db {
@@ -6,12 +5,64 @@ class Db {
   static final Db instance = Db._();
   static Database? _database;
 
-  Database get database {
-    if (_database != null) return _database!;
-    else throw Exception("Database is not initialized. Call Db.instance.initDb() first.");
+  Database? get database => _database;
+
+  Future<void> openDb(String path) async {
+    _database = await openDatabase(path);
   }
 
-  Future<void> initDb(String path) async {
-    _database = await openDatabase(path);
+  Future<void> createDb(String path) async {
+    _database = await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute("""
+        CREATE TABLE note (
+          note_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          data TEXT NOT NULL,
+          author VARCHAR(64) NOT NULL DEFAULT '',
+          client VARCHAR(255) NOT NULL DEFAULT '',
+          user_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          colour INTEGER NOT NULL DEFAULT 16777215,
+          rank TINYINT NOT NULL DEFAULT 0,
+          is_visible BOOLEAN NOT NULL DEFAULT true,
+          is_favourite BOOLEAN NOT NULL DEFAULT false,
+          is_deleted BOOLEAN NOT NULL DEFAULT false,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );""");
+
+      await db.execute("""  
+        CREATE TABLE tag (
+          tag_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          name VARCHAR(64) UNIQUE NOT NULL,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );""");
+
+      await db.execute("""  
+        CREATE TABLE image (
+          guid UUID PRIMARY KEY NOT NULL,
+          data BLOB NOT NULL,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );""");
+
+      await db.execute("""  
+        CREATE TABLE note_to_tag (
+          note_id INTEGER NOT NULL REFERENCES note (note_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+          tag_id INTEGER NOT NULL REFERENCES tag (tag_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (note_id, tag_id)
+        );""");
+
+      await db.execute("""  
+        CREATE TABLE metadata (
+          key VARCHAR(64) PRIMARY KEY NOT NULL,
+          value VARCHAR(255) NOT NULL,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );""");
+
+      await db.execute("INSERT INTO metadata (key, value) VALUES ('author', 'Artem Mitrakov, https://github.com/mitrakov, mitrakov-artem@yandex.ru'), ('schema_version', '1');");
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: curly_braces_in_flow_control_structures
+import 'dart:io';
 import 'dart:math';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -11,22 +12,10 @@ import 'package:tommynotes/trixcontainer.dart';
 
 const String lastOpenPathKey = "LAST_OPEN_PATH";
 
-void main() async {
-  // debugPaintSizeEnabled = true;
+void main() {
   WidgetsFlutterBinding.ensureInitialized(); // allow async code in main()
-  await Settings.instance.init();
-  final path = Settings.instance.settings.getString(lastOpenPathKey) ?? await getStartFile();
-  await Db.instance.initDb(path);
-
+  Settings.instance.init();
   runApp(const MyApp());
-}
-
-Future<String> getStartFile() async {
-  final FilePickerResult? res = await FilePicker.platform.pickFiles(dialogTitle: "Select a DB file", type: FileType.custom, allowedExtensions: ["db"], lockParentWindow: true);
-  final result = res?.files.first.path ?? "";
-  if (result.isNotEmpty)
-    Settings.instance.settings.setString(lastOpenPathKey, result);
-  return result;
 }
 
 class MyApp extends StatelessWidget {
@@ -62,102 +51,117 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        leading: IconButton(icon: const Icon(Icons.add_box_rounded), onPressed: () => setState(() {
-          _noteId = 0;
-          _mainCtrl.text = "";
-          _tagsCtrl.text = "";
-        })),
-      ),
-      body: Column(
-        children: [
-          Flexible(
-            flex: 8,
-            child: Row(
-              children: [
-                Expanded(
-                  child: TrixContainer(
-                    child: FutureBuilder(
-                      future: _getTags(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final tags = snapshot.data!.map((tag) => Text(tag)).toList();
-                          return ListView(children: [const Text("Tags", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)), ...tags]);
-                        } else return const CircularProgressIndicator();
-                      },
+    return PlatformMenuBar(
+      menus: [
+        PlatformMenu(
+          label: "Hey-Hey",
+          menus: [
+            PlatformMenuItemGroup(members: [
+              PlatformMenuItem(label: "New DB file", onSelected: _newDbFile),
+              PlatformMenuItem(label: "Open DB file", onSelected: _openDbFile),
+              PlatformMenuItem(label: "Open latest DB file", onSelected: _openLatestDbFile),
+            ]),
+            PlatformMenuItem(label: "Quit", onSelected: () => exit(0)),
+          ],
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          leading: IconButton(icon: const Icon(Icons.add_box_rounded), onPressed: () => setState(() {
+            _noteId = 0;
+            _mainCtrl.text = "";
+            _tagsCtrl.text = "";
+          })),
+        ),
+        body: Db.instance.database == null ? const Center(child: Text("Welcome!\nOpen or create a new DB file")) : Column(
+          children: [
+            Flexible(
+              flex: 8,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TrixContainer(
+                      child: FutureBuilder(
+                        future: _getTags(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final tags = snapshot.data!.map((tag) => Text(tag)).toList();
+                            return ListView(children: [const Text("Tags", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)), ...tags]);
+                          } else return const CircularProgressIndicator();
+                        },
+                      ),
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 6,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TrixContainer(
-                                child: TextField(controller: _mainCtrl, maxLines: 1024, onChanged: (s) => setState(() {})),
+                  Flexible(
+                    flex: 6,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TrixContainer(
+                                  child: TextField(controller: _mainCtrl, maxLines: 1024, onChanged: (s) => setState(() {})),
+                                ),
                               ),
-                            ),
-                            Expanded(child: TrixContainer(child: MarkdownWidget(data: _mainCtrl.text))),
-                          ],
+                              Expanded(child: TrixContainer(child: MarkdownWidget(data: _mainCtrl.text))),
+                            ],
+                          ),
                         ),
-                      ),
-                      TrixContainer(
-                        child: Row(children: [
-                          SizedBox(width: 300, child: TextField(controller: _tagsCtrl, decoration: const InputDecoration(label: Text("Tags")))),
-                          OutlinedButton(onPressed: _save, child: Text(_noteId == 0 ? "Add New" : "Save")),
-                        ]),
-                      ),
-                    ],
+                        TrixContainer(
+                          child: Row(children: [
+                            SizedBox(width: 300, child: TextField(controller: _tagsCtrl, decoration: const InputDecoration(label: Text("Tags")))),
+                            OutlinedButton(onPressed: _saveNote, child: Text(_noteId == 0 ? "Add New" : "Save")),
+                          ]),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: TrixContainer(
-              child: FutureBuilder(
-                future: _getNotes(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final futureResult = snapshot.data!;
-                    final children = futureResult.map((note) =>
-                      TrixContainer(
-                        child: TextButton(child: Text(note.note.substring(0, min(note.note.length, 32))), onPressed: () => setState(() {
-                          _noteId = note.noteId;
-                          _mainCtrl.text = note.note;
-                          _tagsCtrl.text = note.tags;
-                        }))
-                      )).toList(); // TODO 32 is total char count which is wrong
-                    return ListView(scrollDirection: Axis.horizontal, children: children); // TODO use ListView.builder
-                  } else return const CircularProgressIndicator();
-                },
+                ],
               ),
             ),
-          )
-        ],
+            Expanded(
+              child: TrixContainer(
+                child: FutureBuilder(
+                  future: _getNotes(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final futureResult = snapshot.data!;
+                      final children = futureResult.map((note) =>
+                        TrixContainer(
+                          child: TextButton(child: Text(note.note.substring(0, min(note.note.length, 32))), onPressed: () => setState(() {
+                            _noteId = note.noteId;
+                            _mainCtrl.text = note.note;
+                            _tagsCtrl.text = note.tags;
+                          }))
+                        )).toList(); // TODO 32 is total char count which is wrong
+                      return ListView(scrollDirection: Axis.horizontal, children: children); // TODO use ListView.builder
+                    } else return const CircularProgressIndicator();
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
   Future<Iterable<Note>> _getNotes() async {
-    final dbResult = await Db.instance.database.rawQuery(
+    final dbResult = await Db.instance.database!.rawQuery(
       "SELECT note_id, data, GROUP_CONCAT(name, ', ') AS tags FROM note INNER JOIN note_to_tag USING (note_id) INNER JOIN tag USING (tag_id) GROUP BY note_id;"
     );
     return dbResult.map((e) => Note(noteId: int.parse(e["note_id"].toString()), note: e["data"].toString(), tags: e["tags"].toString()));
   }
 
   Future<Iterable<String>> _getTags() async {
-    final dbResult = await Db.instance.database.rawQuery("SELECT name FROM tag;");
+    final dbResult = await Db.instance.database!.rawQuery("SELECT name FROM tag;");
     return dbResult.map((e) => e["name"].toString());
   }
 
-  void _save() async {
+  void _saveNote() async {
     final data = _mainCtrl.text.trim();
     final tags = _tagsCtrl.text.split(",").map((tag) => tag.trim()).where((tag) => tag.isNotEmpty);
     if (tags.isEmpty) {
@@ -166,29 +170,62 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     if (data.isNotEmpty) {
       if (_noteId == 0) { // INSERT
-        final newNoteId = await Db.instance.database.rawInsert("INSERT INTO note (data) VALUES (?);", [data]);
+        final newNoteId = await Db.instance.database!.rawInsert("INSERT INTO note (data) VALUES (?);", [data]);
         tags.forEach((tag) async {
           late final int tagId;
-          final tagIdOpt = await Db.instance.database.rawQuery("SELECT tag_id FROM tag WHERE name = ?;", [tag]);
+          final tagIdOpt = await Db.instance.database!.rawQuery("SELECT tag_id FROM tag WHERE name = ?;", [tag]);
           if (tagIdOpt.isNotEmpty) { // Tag exists
             tagId = int.parse(tagIdOpt.first["tag_id"].toString());
           } else {                   // New tag
-            tagId = await Db.instance.database.rawInsert("INSERT INTO tag (name) VALUES (?);", [tag]);
+            tagId = await Db.instance.database!.rawInsert("INSERT INTO tag (name) VALUES (?);", [tag]);
           }
           
-          await Db.instance.database.rawInsert("INSERT INTO note_to_tag (note_id, tag_id) VALUES (?, ?);", [newNoteId, tagId]);
+          await Db.instance.database!.rawInsert("INSERT INTO note_to_tag (note_id, tag_id) VALUES (?, ?);", [newNoteId, tagId]);
         });
+        await FlutterPlatformAlert.showAlert(windowTitle: "Success", text: "New note added");
         setState(() {
           _noteId = newNoteId;
           // text and tags are kept
         });
-        FlutterPlatformAlert.showAlert(windowTitle: "Success", text: "New note added");
       } else { // UPDATE
-        await Db.instance.database.rawUpdate("UPDATE note SET data = ? WHERE note_id = ?;", [data, _noteId]);
+        await Db.instance.database!.rawUpdate("UPDATE note SET data = ? WHERE note_id = ?;", [data, _noteId]);
         // TODO update tags
-        setState(() {}); // repaint
-        FlutterPlatformAlert.showAlert(windowTitle: "Success", text: "Updated");
+        await FlutterPlatformAlert.showAlert(windowTitle: "Success", text: "Updated");
+        setState(() {}); // refresh
       }
     }
+  }
+
+  void _newDbFile() async {
+    final path = await FilePicker.platform.saveFile(dialogTitle: "Create a new DB file", fileName: "workspace.db", allowedExtensions: ["db"], lockParentWindow: true);
+    if (path != null) {
+      Settings.instance.settings.setString(lastOpenPathKey, path);
+      await Db.instance.createDb(path);
+      setState(() {}); // refresh
+    }
+  }
+
+  void _openDbFile() async {
+    final path = await _getStartFile();
+    if (path != null) {
+      await Db.instance.openDb(path);
+      setState(() {}); // refresh
+    }
+  }
+
+  void _openLatestDbFile() async {
+    final path = Settings.instance.settings.getString(lastOpenPathKey) ?? await _getStartFile();
+    if (path != null) {
+      await Db.instance.openDb(path);
+      setState(() {}); // refresh
+    }
+  }
+
+  Future<String?> _getStartFile() async {
+    final FilePickerResult? res = await FilePicker.platform.pickFiles(dialogTitle: "Select a DB file", type: FileType.custom, allowedExtensions: ["db"], lockParentWindow: true);
+    final path = res?.files.first.path;
+    if (path != null)
+      Settings.instance.settings.setString(lastOpenPathKey, path);
+    return path;
   }
 }
